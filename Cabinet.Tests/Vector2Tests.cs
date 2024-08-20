5b758f5659e5570673f7a2f9741cef702ceda4f0
sttaaasssiik <sttaaasssiik@yandex.ru>
2024-08-20 13:42:34 +0500

    add Vector2Tests

---
+++ Cabinet.Tests/Vector2Tests.cs
@@ -0,0 +1,12 @@
+﻿namespace Cabinet.Tests;
+
+public class Vector2Tests
+{
+    //[Fact]
+    //public void FromRGB_Should_Return_Correct_Color()
+    //{
+    //    var result = Color.FromRgba(0, 0, 0, 0);
+
+    //    Assert.Equal(Color.FromRgba(0, 0, 0, 0), result);
+    //}
+}
