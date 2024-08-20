2d7598fdd5a9540ddd280237b40138cfb1341563
sttaaasssiik <sttaaasssiik@yandex.ru>
2024-08-20 13:35:11 +0500

    add Cabinet.Tests

---
+++ Cabinet.Tests/ColorTests.cs
@@ -0,0 +1,12 @@
+namespace Cabinet.Tests;
+
+public class ColorTests
+{
+    [Fact]
+    public void FromRGB_Should_Return_Correct_Color()
+    {
+        var result = Color.FromRgba(0, 0, 0, 0);
+
+        Assert.Equal(Color.FromRgba(0, 0, 0, 0), result);
+    }
+}
