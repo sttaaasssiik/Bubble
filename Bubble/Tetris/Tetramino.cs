e7b48949c04915edd95f63e89e4526d65e72406e
sttaaasssiik <sttaaasssiik@yandex.ru>
2024-08-22 09:55:49 +0500

    Tetramino refac

--- Bubble/Tetris/Tetramino.cs
+++ Bubble/Tetris/Tetramino.cs
@@ -2,36 +2,35 @@
 
 namespace Bubble.Tetris;
 
-public class Tetramino(TetraminoAssetExtractor tetraminoData, int kind, Vector2 position, int rotation)
+public class Tetramino(TetraminoAssetExtractor tetraminoData, int kind, Vector2 startingPosition, int rotation)
 {
     private readonly TetraminoAssetExtractor extractor = tetraminoData;
     private readonly int kind = kind;
-
-    private Vector2 position = position;
     private int rotation = rotation;
 
-    public Vector2 Position => position;
+    public Vector2 Position { get; private set; } = startingPosition;
 
     public IEnumerable<Block> Blocks => extractor[kind, rotation]
-        .Select(x => x with { Position = position + x.Position });
+        .Select(x => (x with { Position = Position + x.Position }));
 
-    public Tetramino Clone() => new(extractor, kind, position, rotation);
+    public Tetramino Clone() => new(extractor, kind, Position, rotation);
 
     public void Rotate() =>
         rotation = rotation < extractor.GetRotationsForKindCount(kind) - 1
         ? rotation + 1
         : 0;
 
-    public void MoveDown() => position -= Vector2.Up;
+    public void MoveDown() => Position -= Vector2.Up;
 
-    public void MoveLeft() => position -= Vector2.Right;
+    public void MoveLeft() => Position -= Vector2.Right;
 
-    public void MoveRight() => position += Vector2.Right;
+    public void MoveRight() => Position += Vector2.Right;
 
     public bool IsCollided(IEnumerable<Block> blocks)
     {
         static bool equals(Block x, Block y) => x.Position == y.Position;
-        var blockPositionComparer = EqualityComparer<Block>.Create(equals, b => 0);
+        static int getHashCode(Block b) => 0;
+        var blockPositionComparer = EqualityComparer<Block>.Create(equals, getHashCode);
         return Blocks.Intersect(blocks, blockPositionComparer).Any();
     }
 }
