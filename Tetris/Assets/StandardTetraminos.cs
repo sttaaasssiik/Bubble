01b2500b9252c39e81380892e42e5b50cc5b5166
sttaaasssiik <sttaaasssiik@yandex.ru>
2024-08-23 16:21:45 +0500

    cosmetic

--- Tetris/Assets/StandardTetraminos.cs
+++ Tetris/Assets/StandardTetraminos.cs
@@ -15,194 +15,194 @@ public static class StandardTetraminos
     };
 
     public static List<List<Block>> ITetramino => new()
+    {
+        new ()
         {
-            new ()
-            {
-                new("I", new Vector2(0, 0)),
-                new("I", new Vector2(0, -1)),
-                new("I", new Vector2(0, -2)),
-                new("I", new Vector2(0, -3)),
-            },
-            new ()
-            {
-                new("I", new Vector2(-2, -1)),
-                new("I", new Vector2(-1, -1)),
-                new("I", new Vector2(0, -1)),
-                new("I", new Vector2(1, -1))
-            },
-            new ()
-            {
-                new("I", new Vector2(0, 0)),
-                new("I", new Vector2(0, -1)),
-                new("I", new Vector2(0, -2)),
-                new("I", new Vector2(0, -3)),
-            },
-            new ()
-            {
-                new("I", new Vector2(-2, -1)),
-                new("I", new Vector2(-1, -1)),
-                new("I", new Vector2(0, -1)),
-                new("I", new Vector2(1, -1))
-            }
-        };
+            new("I", new Vector2(0, 0)),
+            new("I", new Vector2(0, -1)),
+            new("I", new Vector2(0, -2)),
+            new("I", new Vector2(0, -3)),
+        },
+        new ()
+        {
+            new("I", new Vector2(-2, -1)),
+            new("I", new Vector2(-1, -1)),
+            new("I", new Vector2(0, -1)),
+            new("I", new Vector2(1, -1))
+        },
+        new ()
+        {
+            new("I", new Vector2(0, 0)),
+            new("I", new Vector2(0, -1)),
+            new("I", new Vector2(0, -2)),
+            new("I", new Vector2(0, -3)),
+        },
+        new ()
+        {
+            new("I", new Vector2(-2, -1)),
+            new("I", new Vector2(-1, -1)),
+            new("I", new Vector2(0, -1)),
+            new("I", new Vector2(1, -1))
+        }
+    };
 
     public static List<List<Block>> OTetramino => new()
+    {
+        new()
         {
-            new()
-            {
-                new("O", new Vector2(0, 0)),
-                new("O", new Vector2(0, -1)),
-                new("O", new Vector2(1, 0)),
-                new("O", new Vector2(1, -1)),
-            },
-            new()
-            {
-                new("O", new Vector2(0, 0)),
-                new("O", new Vector2(0, -1)),
-                new("O", new Vector2(1, 0)),
-                new("O", new Vector2(1, -1)),
-            },
-            new()
-            {
-                new("O", new Vector2(0, 0)),
-                new("O", new Vector2(0, -1)),
-                new("O", new Vector2(1, 0)),
-                new("O", new Vector2(1, -1)),
-            },
-            new()
-            {
-                new("O", new Vector2(0, 0)),
-                new("O", new Vector2(0, -1)),
-                new("O", new Vector2(1, 0)),
-                new("O", new Vector2(1, -1)),
-            },
-        };
+            new("O", new Vector2(0, 0)),
+            new("O", new Vector2(0, -1)),
+            new("O", new Vector2(1, 0)),
+            new("O", new Vector2(1, -1)),
+        },
+        new()
+        {
+            new("O", new Vector2(0, 0)),
+            new("O", new Vector2(0, -1)),
+            new("O", new Vector2(1, 0)),
+            new("O", new Vector2(1, -1)),
+        },
+        new()
+        {
+            new("O", new Vector2(0, 0)),
+            new("O", new Vector2(0, -1)),
+            new("O", new Vector2(1, 0)),
+            new("O", new Vector2(1, -1)),
+        },
+        new()
+        {
+            new("O", new Vector2(0, 0)),
+            new("O", new Vector2(0, -1)),
+            new("O", new Vector2(1, 0)),
+            new("O", new Vector2(1, -1)),
+        },
+    };
 
     public static List<List<Block>> LTetramino => new()
+    {
+        new()
         {
-            new()
-            {
-                new("L", new Vector2(0, 0)),
-                new("L", new Vector2(0, -1)),
-                new("L", new Vector2(0, -2)),
-                new("L", new Vector2(1, -2)),
-            },
-            new()
-            {
-                new("L", new Vector2(-1, -1)),
-                new("L", new Vector2(0, -1)),
-                new("L", new Vector2(1, -1)),
-                new("L", new Vector2(-1, -2)),
-            },
-            new()
-            {
-                new("L", new Vector2(0, -2)),
-                new("L", new Vector2(0, -1)),
-                new("L", new Vector2(0, 0)),
-                new("L", new Vector2(-1, 0)),
-            },
-            new()
-            {
-                new("L", new Vector2(-1, -1)),
-                new("L", new Vector2(0, -1)),
-                new("L", new Vector2(1, -1)),
-                new("L", new Vector2(1, 0)),
-            },
-        };
+            new("L", new Vector2(0, 0)),
+            new("L", new Vector2(0, -1)),
+            new("L", new Vector2(0, -2)),
+            new("L", new Vector2(1, -2)),
+        },
+        new()
+        {
+            new("L", new Vector2(-1, -1)),
+            new("L", new Vector2(0, -1)),
+            new("L", new Vector2(1, -1)),
+            new("L", new Vector2(-1, -2)),
+        },
+        new()
+        {
+            new("L", new Vector2(0, -2)),
+            new("L", new Vector2(0, -1)),
+            new("L", new Vector2(0, 0)),
+            new("L", new Vector2(-1, 0)),
+        },
+        new()
+        {
+            new("L", new Vector2(-1, -1)),
+            new("L", new Vector2(0, -1)),
+            new("L", new Vector2(1, -1)),
+            new("L", new Vector2(1, 0)),
+        },
+    };
 
     public static List<List<Block>> JTetramino => new()
+    {
+        new()
         {
-            new()
-            {
-                new("J", new Vector2(0, 0)),
-                new("J", new Vector2(0, -1)),
-                new("J", new Vector2(0, -2)),
-                new("J", new Vector2(-1, -2)),
-            },
-            new()
-            {
-                new("J", new Vector2(-1, 0)),
-                new("J", new Vector2(-1, -1)),
-                new("J", new Vector2(0, -1)),
-                new("J", new Vector2(1, -1)),
-            },
-            new()
-            {
-                new("J", new Vector2(0, 0)),
-                new("J", new Vector2(1, 0)),
-                new("J", new Vector2(0, -1)),
-                new("J", new Vector2(0, -2)),
-            },
-            new()
-            {
-                new("J", new Vector2(-1, -1)),
-                new("J", new Vector2(0, -1)),
-                new("J", new Vector2(1, -1)),
-                new("J", new Vector2(1, -2)),
-            },
-        };
+            new("J", new Vector2(0, 0)),
+            new("J", new Vector2(0, -1)),
+            new("J", new Vector2(0, -2)),
+            new("J", new Vector2(-1, -2)),
+        },
+        new()
+        {
+            new("J", new Vector2(-1, 0)),
+            new("J", new Vector2(-1, -1)),
+            new("J", new Vector2(0, -1)),
+            new("J", new Vector2(1, -1)),
+        },
+        new()
+        {
+            new("J", new Vector2(0, 0)),
+            new("J", new Vector2(1, 0)),
+            new("J", new Vector2(0, -1)),
+            new("J", new Vector2(0, -2)),
+        },
+        new()
+        {
+            new("J", new Vector2(-1, -1)),
+            new("J", new Vector2(0, -1)),
+            new("J", new Vector2(1, -1)),
+            new("J", new Vector2(1, -2)),
+        },
+    };
 
     public static List<List<Block>> STetramino => new()
+    {
+        new()
         {
-            new()
-            {
-                new("S", new Vector2(0, 0)),
-                new("S", new Vector2(1, 0)),
-                new("S", new Vector2(0, -1)),
-                new("S", new Vector2(-1, -1)),
-            },
-            new()
-            {
-                new("S", new Vector2(0, 1)),
-                new("S", new Vector2(0, 0)),
-                new("S", new Vector2(1, 0)),
-                new("S", new Vector2(1, -1)),
-            },
-            new()
-            {
-                new("S", new Vector2(0, 0)),
-                new("S", new Vector2(1, 0)),
-                new("S", new Vector2(0, -1)),
-                new("S", new Vector2(-1, -1)),
-            },
-            new()
-            {
-                new("S", new Vector2(0, 1)),
-                new("S", new Vector2(0, 0)),
-                new("S", new Vector2(1, 0)),
-                new("S", new Vector2(1, -1)),
-            },
-        };
+            new("S", new Vector2(0, 0)),
+            new("S", new Vector2(1, 0)),
+            new("S", new Vector2(0, -1)),
+            new("S", new Vector2(-1, -1)),
+        },
+        new()
+        {
+            new("S", new Vector2(0, 1)),
+            new("S", new Vector2(0, 0)),
+            new("S", new Vector2(1, 0)),
+            new("S", new Vector2(1, -1)),
+        },
+        new()
+        {
+            new("S", new Vector2(0, 0)),
+            new("S", new Vector2(1, 0)),
+            new("S", new Vector2(0, -1)),
+            new("S", new Vector2(-1, -1)),
+        },
+        new()
+        {
+            new("S", new Vector2(0, 1)),
+            new("S", new Vector2(0, 0)),
+            new("S", new Vector2(1, 0)),
+            new("S", new Vector2(1, -1)),
+        },
+    };
 
     public static List<List<Block>> ZTetramino => new()
+    {
+        new()
         {
-            new()
-            {
-                new("Z", new Vector2(0, 0)),
-                new("Z", new Vector2(1, 0)),
-                new("Z", new Vector2(1, -1)),
-                new("Z", new Vector2(2, -1)),
-            },
-            new()
-            {
-                new("Z", new Vector2(2, 1)),
-                new("Z", new Vector2(2, 0)),
-                new("Z", new Vector2(1, 0)),
-                new("Z", new Vector2(1, -1)),
-            },
-            new()
-            {
-                new("Z", new Vector2(0, 0)),
-                new("Z", new Vector2(1, 0)),
-                new("Z", new Vector2(1, -1)),
-                new("Z", new Vector2(2, -1)),
-            },
-            new()
-            {
-                new("Z", new Vector2(2, 1)),
-                new("Z", new Vector2(2, 0)),
-                new("Z", new Vector2(1, 0)),
-                new("Z", new Vector2(1, -1)),
-            },
-        };
+            new("Z", new Vector2(0, 0)),
+            new("Z", new Vector2(1, 0)),
+            new("Z", new Vector2(1, -1)),
+            new("Z", new Vector2(2, -1)),
+        },
+        new()
+        {
+            new("Z", new Vector2(2, 1)),
+            new("Z", new Vector2(2, 0)),
+            new("Z", new Vector2(1, 0)),
+            new("Z", new Vector2(1, -1)),
+        },
+        new()
+        {
+            new("Z", new Vector2(0, 0)),
+            new("Z", new Vector2(1, 0)),
+            new("Z", new Vector2(1, -1)),
+            new("Z", new Vector2(2, -1)),
+        },
+        new()
+        {
+            new("Z", new Vector2(2, 1)),
+            new("Z", new Vector2(2, 0)),
+            new("Z", new Vector2(1, 0)),
+            new("Z", new Vector2(1, -1)),
+        },
+    };
 }
