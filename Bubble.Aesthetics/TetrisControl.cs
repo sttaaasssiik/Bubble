b1f31750909dfb88bbb25c92bb0885e7c1a067d7
sttaaasssiik <sttaaasssiik@yandex.ru>
2024-08-31 14:41:36 +0500

    add FieldChanged

--- Bubble.Aesthetics/TetrisControl.cs
+++ Bubble.Aesthetics/TetrisControl.cs
@@ -12,70 +12,73 @@ namespace Bubble.Aesthetics;
 
 public class TetrisControl : Control
 {
-    private readonly TetrisGame tetrisGame = StandardTetrisGame.Create();
+	private readonly TetrisGame tetrisGame = StandardTetrisGame.Create();
 
-    private readonly Dictionary<Key, Action<TetrisGame>> keyboard = new()
-    {
-        [Key.Up] = tetrisGame => tetrisGame.Rotate(),
-        [Key.Right] = tetrisGame => tetrisGame.MoveRight(),
-        [Key.Down] = tetrisGame => tetrisGame.MoveDown(),
-        [Key.Left] = tetrisGame => tetrisGame.MoveLeft(),
-    };
+	private readonly Dictionary<Key, Action<TetrisGame>> keyboard = new()
+	{
+		[Key.Up] = tetrisGame => tetrisGame.Rotate(),
+		[Key.Right] = tetrisGame => tetrisGame.MoveRight(),
+		[Key.Down] = tetrisGame => tetrisGame.MoveDown(),
+		[Key.Left] = tetrisGame => tetrisGame.MoveLeft(),
+	};
 
-    private readonly Dictionary<string, Color> colors = new()
-    {
-        ["I"] = Color.FromRgba(230, 35, 35, 255),
-        ["O"] = Color.FromRgba(35, 230, 35, 255),
-        ["L"] = Color.FromRgba(35, 35, 230, 255),
-        ["J"] = Color.FromRgba(200, 200, 200, 255),
-        ["S"] = Color.FromRgba(200, 200, 200, 255),
-        ["Z"] = Color.FromRgba(200, 200, 200, 255),
-    };
+	private readonly Dictionary<string, Color> colors = new()
+	{
+		["I"] = Color.FromRgba(230, 35, 35, 255),
+		["O"] = Color.FromRgba(35, 230, 35, 255),
+		["L"] = Color.FromRgba(35, 35, 230, 255),
+		["J"] = Color.FromRgba(200, 200, 200, 255),
+		["S"] = Color.FromRgba(200, 200, 200, 255),
+		["Z"] = Color.FromRgba(200, 200, 200, 255),
+	};
 
-    private readonly DotMatrixDisplaySimple field = new()
-    {
-        Background = Color.FromRgba(50, 50, 50, 255),
-        Size = new Vector2(10, 20),
-        DotSize = new Vector2(20, 20)
-    };
+	private readonly DotMatrixDisplaySimple field = new()
+	{
+		Background = Color.FromRgba(50, 50, 50, 255),
+		Size = new Vector2(10, 20),
+		DotSize = new Vector2(20, 20)
+	};
 
-    private readonly DotMatrixDisplaySimple linesCounter = new()
-    {
-        LocalPosition = new Vector2(220, 0),
-        Background = Color.FromRgba(50, 50, 50, 255),
-        Size = new Vector2(10, 10),
-        DotSize = new Vector2(10, 10)
-    };
+	private readonly DotMatrixDisplaySimple linesCounter = new()
+	{
+		LocalPosition = new Vector2(220, 0),
+		Background = Color.FromRgba(50, 50, 50, 255),
+		Size = new Vector2(10, 10),
+		DotSize = new Vector2(10, 10)
+	};
 
-    private readonly Timer timer = new(1000);
+	private readonly Timer timer = new(1000);
 
-    public TetrisControl()
-    {
-        timer.Elapsed += (x, y) => tetrisGame.MoveDown();
-        timer.Start();
-    }
+	public TetrisControl()
+	{
+		timer.Elapsed += (x, y) => tetrisGame.MoveDown();
+		timer.Start();
 
-    public override void OnKeyDown(Key key)
-    {
-        keyboard[key](tetrisGame);
-    }
+		static Vector2 TransformPosition(Vector2 pos) => new(pos.X, -pos.Y + 19);
+		Color getColor(string id) =>
+			colors.TryGetValue(id, out var color)
+			? color
+			: Color.FromRgba(0, 0, 255, 255);
 
-    protected override void OnRender(DrawingContext drawingContext)
-    {
-        static Vector2 TransformPosition(Vector2 pos) => new(pos.X, -pos.Y + 19);
-        Color getColor(string id) =>
-            colors.TryGetValue(id, out var color)
-            ? color
-            : Color.FromRgba(0, 0, 255, 255);
+		tetrisGame.FieldChanged += f =>
+		{
+			field.Dots = f.Select(x => (TransformPosition(x.Position), getColor(x.Id)));
+			linesCounter.Dots = Enumerable
+				.Range(0, tetrisGame.TotalLinesCollapsed)
+				.Select(x => (new Vector2(x, 0), Color.FromRgba(230, 35, 35, 255)));
+		};
+	}
 
-        field.Dots = tetrisGame.Field.Select(x => (TransformPosition(x.Position), getColor(x.Id)));
-        field.Render(drawingContext);
+	public override void OnKeyDown(Key key)
+	{
+		keyboard[key](tetrisGame);
+	}
 
-        linesCounter.Dots = Enumerable
-            .Range(0, tetrisGame.TotalLinesCollapsed)
-            .Select(x => (new Vector2(x, 0), Color.FromRgba(230, 35, 35, 255)));
-        linesCounter.Render(drawingContext);
+	protected override void OnRender(DrawingContext drawingContext)
+	{
+		field.Render(drawingContext);
+		linesCounter.Render(drawingContext);
 
-        SDL.SDL_RenderPresent(drawingContext.Id);
-    }
+		SDL.SDL_RenderPresent(drawingContext.Id);
+	}
 }
--- Tetris/TetrisGame.cs
+++ Tetris/TetrisGame.cs
@@ -4,74 +4,82 @@ namespace Tetris;
 
 public class TetrisGame
 {
-    private readonly IEnumerable<Block> boundaries;
-    private readonly Pile pile = new();
-    private readonly Vector2 startingPosition;
-    private readonly ITetraminoSequence tetraminoSequence;
+	private readonly IEnumerable<Block> boundaries;
+	private readonly Pile pile = new();
+	private readonly Vector2 startingPosition;
+	private readonly ITetraminoSequence tetraminoSequence;
 
-    private Tetramino tetramino;
+	private Tetramino tetramino;
 
-    public TetrisGame(ITetraminoSequence tetraminoSequence, IEnumerable<Block> boundaries, Vector2 startingPosition)
-    {
-        this.tetraminoSequence = tetraminoSequence;
-        this.boundaries = boundaries;
-        this.startingPosition = startingPosition;
-        tetramino = InitTetramino();
-    }
+	public TetrisGame(ITetraminoSequence tetraminoSequence, IEnumerable<Block> boundaries, Vector2 startingPosition)
+	{
+		this.tetraminoSequence = tetraminoSequence;
+		this.boundaries = boundaries;
+		this.startingPosition = startingPosition;
+		tetramino = InitTetramino();
+		FieldChanged?.Invoke(Field);
+	}
 
-    public int LinesCollapsed { get; private set; }
+	public event Action<IEnumerable<Block>>? FieldChanged;
 
-    public int TotalLinesCollapsed { get; private set; }
+	public int LinesCollapsed { get; private set; }
 
-    private IEnumerable<Block> CollidableBlocks => boundaries.Concat(pile.Blocks);
+	public int TotalLinesCollapsed { get; private set; }
 
-    public IEnumerable<Block> Field => tetramino.Blocks.Concat(pile.Blocks);
+	private IEnumerable<Block> CollidableBlocks => boundaries.Concat(pile.Blocks);
 
-    public void Rotate()
-    {
-        Move(x => x.Rotate());
-    }
+	public IEnumerable<Block> Field => tetramino.Blocks.Concat(pile.Blocks);
 
-    public void MoveDown()
-    {
-        var clone = tetramino.Clone();
-        clone.MoveDown();
-        if (clone.IsCollided(CollidableBlocks))
-        {
-            LinesCollapsed = pile.AddRange(tetramino.Blocks);
-            TotalLinesCollapsed += LinesCollapsed;
-            tetramino = InitTetramino();
-        }
-        else
-        {
-            tetramino = clone;
-        }
-    }
+	public void Rotate()
+	{
+		Move(x => x.Rotate());
+		FieldChanged?.Invoke(Field);
+	}
 
-    public void MoveLeft()
-    {
-        Move(x => x.MoveLeft());
-    }
+	public void MoveDown()
+	{
+		var clone = tetramino.Clone();
+		clone.MoveDown();
+		if (clone.IsCollided(CollidableBlocks))
+		{
+			LinesCollapsed = pile.AddRange(tetramino.Blocks);
+			TotalLinesCollapsed += LinesCollapsed;
+			tetramino = InitTetramino();
+		}
+		else
+		{
+			tetramino = clone;
+		}
+		FieldChanged?.Invoke(Field);
+	}
 
-    public void MoveRight()
-    {
-        Move(x => x.MoveRight());
-    }
+	public void MoveLeft()
+	{
+		Move(x => x.MoveLeft());
+		FieldChanged?.Invoke(Field);
+	}
 
-    private void Move(Action<Tetramino> action)
-    {
-        var clone = tetramino.Clone();
-        action(clone);
-        if (!clone.IsCollided(CollidableBlocks))
-        {
-            tetramino = clone;
-        }
-    }
+	public void MoveRight()
+	{
+		Move(x => x.MoveRight());
+		FieldChanged?.Invoke(Field);
+	}
 
-    private Tetramino InitTetramino()
-    {
-        var tetramino = tetraminoSequence.Next;
-        tetramino.Position = startingPosition;
-        return tetramino;
-    }
+	private void Move(Action<Tetramino> action)
+	{
+		var clone = tetramino.Clone();
+		action(clone);
+		if (!clone.IsCollided(CollidableBlocks))
+		{
+			tetramino = clone;
+		}
+		FieldChanged?.Invoke(Field);
+	}
+
+	private Tetramino InitTetramino()
+	{
+		var tetramino = tetraminoSequence.Next;
+		tetramino.Position = startingPosition;
+		return tetramino;
+	}
 }
